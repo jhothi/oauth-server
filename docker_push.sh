0e5335a7333dd7efd0bc75785e350905aaef62e5
jhothi <jaghothi95@gmail.com>
2018-08-16 00:00:35 -0400

    Test change to html output.

--- docker_push.sh
+++ docker_push.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker tag oauth-server spidey345/oauth-server:1.0
-docker push spidey345/oauth-server
+docker tag oauth-server "$DOCKER_USERNAME"/oauth-server:1.0
+docker push "$DOCKER_USERNAME"/oauth-server
